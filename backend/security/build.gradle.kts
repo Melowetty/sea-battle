eac5b7444bed222888540e30ea847a7b05bf5bfb
Denis Malinin <melowetty@mail.ru>
2025-06-08 23:56:26 +0300

    add jwt deps

--- backend/security/build.gradle.kts
+++ backend/security/build.gradle.kts
@@ -1,7 +1,10 @@
 dependencies {
+    implementation(libs.jwt.api)
     implementation(libs.spring.validation)
     implementation(libs.spring.boot.starter.web)
     implementation(libs.spring.websocket)
     implementation(libs.jackson.kotlin)
+    runtimeOnly(libs.jwt.api)
+    runtimeOnly(libs.jwt.jackson)
     testImplementation(libs.spring.boot.starter.test)
 }
