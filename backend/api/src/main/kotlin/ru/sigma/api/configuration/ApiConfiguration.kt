d99b4e3064d9b416f99988c1784ba7a931336e26
Denis Malinin <melowetty@mail.ru>
2025-06-12 02:06:49 +0300

    move component scan to api module

---
+++ backend/api/src/main/kotlin/ru/sigma/api/configuration/ApiConfiguration.kt
@@ -0,0 +1,8 @@
+package ru.sigma.api.configuration
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ComponentScan("ru.sigma.api")
+class ApiConfiguration
--- backend/src/main/kotlin/ru/sigma/seabattle/SeaBattleApplication.kt
+++ backend/src/main/kotlin/ru/sigma/seabattle/SeaBattleApplication.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan("ru.sigma.api")
 @ComponentScan("ru.sigma.data")
 class SeaBattleApplication
 
